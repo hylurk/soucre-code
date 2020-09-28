0e02cc65f6c62e26666fb15b0bfa7e1091506dab
hylurk <hylurk@163.com>
2020-09-28 18:34:26 +0800

    feat: update

--- react-source/src/index.js
+++ react-source/src/index.js
@@ -49,64 +49,89 @@ import './index.css'
 //   document.getElementById('root')
 // )
 
-// 5. 支持类组件设置状态并且使用 setState() 改变状态
-class SubWelcome extends React.Component {
-  render () {
+// 5. 支持类组件设置状态并且使用 setState() 改变状态以及回调函数
+// class SubWelcome extends React.Component {
+//   render () {
+//     return (
+//     <span>{this.props.count}</span>
+//     )
+//   }
+// }
+// class Welcome extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     // 类组件的状态必须声明在构造函数中
+//     this.state = {
+//       date: new Date(),
+//       count: 0
+//     }
+//   }
+//   componentDidMount () {
+//     this.timer = setInterval(() => {
+//       // 改变状态会使视图更新
+//       this.setState({
+//         date: new Date()
+//       })
+//     }, 1000)
+//   }
+//   handleClick = () => {
+//     // setState 具有合并属性，以及异步的效果
+//     this.setState({
+//       count: this.state.count + 1
+//     }, () => {
+//       console.log(this.state.count)
+//     }) 
+//     console.log(this.state.count)
+//     this.setState({
+//       count: this.state.count + 1
+//     }, () => {
+//       console.log(this.state.count)
+//     })
+//     console.log(this.state.count)
+//   }
+//   render () {
+//     return (
+//       <div className="title">
+//         <h2>你好，{this.props.name}</h2>
+//         <h3>我是标题</h3>
+//         <h4>当前时间：{this.state.date.toLocaleTimeString()}</h4>
+//         <p>你已经点了我 <span>{this.state.count}</span> 次</p>
+//         <button onClick={this.handleClick}>点我将加 1</button>
+//         <p>子组件收到了点击 <SubWelcome count={this.state.count}/> 次</p>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.render(
+//   <Welcome name="陛下" />,
+//   document.getElementById('root'),
+//   () => {
+//     console.log(22)
+//   }
+// )
+
+// 6. 支持 ref 属性，ref 可以是字符串、函数、对象
+class Calculator extends React.Component {
+  handleAdd = () => {
+    const num1 = this.refs.num1.value
+    const num2 = this.refs.num2.value
+    this.refs.count.value = parseFloat(num1) + parseFloat(num2)
+  }
+  render() {
     return (
-    <span>{this.props.count}</span>
-    )
-  }
-}
-class Welcome extends React.Component {
-  constructor (props) {
-    super(props)
-    // 类组件的状态必须声明在构造函数中
-    this.state = {
-      date: new Date(),
-      count: 0
-    }
-  }
-  componentDidMount () {
-    this.timer = setInterval(() => {
-      // 改变状态会使视图更新
-      this.setState({
-        date: new Date()
-      })
-    }, 1000)
-  }
-  handleClick = () => {
-    // setState 具有合并属性，以及异步的效果
-    this.setState({
-      count: this.state.count + 1
-    }, () => {
-      console.log(this.state.count)
-    }) 
-    console.log(this.state.count)
-    this.setState({
-      count: this.state.count + 1
-    }, () => {
-      console.log(this.state.count)
-    })
-    console.log(this.state.count)
-  }
-  render () {
-    return (
-      <div className="title">
-        <h2>你好，{this.props.name}</h2>
-        <h3>我是标题</h3>
-        <h4>当前时间：{this.state.date.toLocaleTimeString()}</h4>
-        <p>你已经点了我 <span>{this.state.count}</span> 次</p>
-        <button onClick={this.handleClick}>点我将加 1</button>
-        <p>子组件收到了点击 <SubWelcome count={this.state.count}/> 次</p>
+      <div style={{ padding: '50px 10px' }}>
+        <input ref="num1" />
+        +
+        <input ref="num2" />
+        <button style={{width: '50px'}} onClick={this.handleAdd}> = </button>
+        <input ref="count" />
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Welcome name="陛下" />,
-  document.getElementById('root'),
-  () => {
-    console.log(22)
-  }
+  <Calculator />,
+  document.getElementById('root')
 )
